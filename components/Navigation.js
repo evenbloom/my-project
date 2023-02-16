55678f75b82abc5a8154e7faa361644e7915edb6
evenbloom <hello@evenbloom.com>
2023-02-16 21:20:29 +0000

    Navigation animation update

--- components/Navigation.js
+++ components/Navigation.js
@@ -5,7 +5,9 @@ import { HamburgerIcon } from './icons/Hamburger'
 import Link from 'next/link'
 import classNames from 'classnames';
 import RichText from './RichText';
-import { motion } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer'
+
 
 export function Navigation({ navigation }) {
   const [hamburgerMenuIsOpen, setHamburgerMenuIsOpen] = useState(false);
@@ -28,11 +30,53 @@ export function Navigation({ navigation }) {
   }, [setHamburgerMenuIsOpen]);
   
   return (
-    <header className='fixed z-10 left-0 right-0 md:top-2 top-0 backdrop-blur-[15px] bg-darkteal md:rounded-[10rem] rounded-0  bg-opacity-20 md:w-[1200px] md:mx-auto md:inline-block'>
+    <header className='fixed z-10 left-0 right-0 md:top-2 top-0 backdrop-blur-[15px] bg-darkteal md:rounded-[10rem] rounded-0 bg-opacity-20 md:w-[1200px] md:mx-auto md:inline-block'>
   
       
           {/* Renders top-level links. */}
-          {navigation.data.slices.map((slice) => {
+    {navigation.data.slices.map((slice) => {
+      const { ref, inView } = useInView({
+    threshold: 0.1
+  });
+  const animation = useAnimation();
+  const subheaderAnimation = useAnimation();
+
+  useEffect(() => {
+    console.log("use effect hook, inView = ", inView);
+    if (inView) {
+      animation.start({
+        y: 0,
+        opacity: 1,
+        transition: {
+          ease: [0.2, 0.8, 0.4, 1], duration: 0.8, delay: 0.28
+        }
+      });
+      subheaderAnimation.start({
+        y: 0,
+        opacity: 1,
+        transition: {
+          ease: [0.2, 0.8, 0.4, 1], duration: 0.8, delay: 0.38
+        }
+      });
+    }
+    if (!inView) {
+      animation.start({
+        y: 100,
+        opacity: 0,
+        transition: {
+          ease: [0.2, 0.8, 0.4, 1], duration: 0.8, delay: 0.38
+        }
+      });
+      subheaderAnimation.start({
+        y: 100,
+        opacity: 0,
+        transition: {
+          ease: [0.2, 0.8, 0.4, 1], duration: 0.8, delay: 0.38
+        }
+      });
+    }
+  }, [inView]);
+      
             return (
               <Container className="flex h-navigation-height justify-between">
                 <Link href="/" className='flex items-center text-md'>  
@@ -46,35 +90,35 @@ export function Navigation({ navigation }) {
 
                 <nav
                   className={classNames(
-                  'h-[100vh] transition-opacity duration-500 fixed -z-10 md:-z-0 top-0 left-0 w-full pr-6 text-background bg-teal overflow-auto md:overflow-hidden md:block md:relative md:top-0 md:h-auto md:w-auto md:translate-x-0 md:bg-transparent md:opacity-100 md:transition-none md:text-lightteal',
+                  'h-[100vh] transition-opacity duration-500 fixed -z-10 md:-z-0 top-0 left-0 w-full pr-6 text-background bg-teal overflow-auto md:overflow-hidden md:block md:relative md:top-0 md:h-auto md:w-auto md:translate-x-0  md:bg-transparent md:opacity-100 md:transition-none md:text-lightteal',
                   hamburgerMenuIsOpen
                 ? "translate-x-0 opacity-100"
                 : "translate-x-[-100vw] opacity-0"
             )}
                 >
-                  <ul className='flex flex-col md:flex-row md:items-center text-md h-full [&_li]:ml-6 md:text-md text-8xl [&_li]:h-[25vh] [&_li]:flex [&_li]:items-center [&_li]:font-sans md:[&_li]:font-sans md:[&_li]:text-md [&_a]:md:transition-colors",
+                  <ul ref={ref} className='flex flex-col md:flex-row md:items-center text-md h-full [&_li]:ml-6 md:text-md text-8xl [&_li]:h-[25vh] [&_li]:pt-[20rem] md:[&_li]:pt-[0rem] [&_li]:flex [&_li]:items-center [&_li]:font-sans md:[&_li]:font-sans md:[&_li]:text-md [&_a]:md:transition-colors",
                 hamburgerMenuIsOpen && "[&_a]:translate-y-0' > 
                     
-                    <li>
+                    <motion.li animate={animation}>
                       <PrismicLink field={slice.primary.link} className='group text-lightteal transition duration-300'>
                         <RichText field={slice.primary.name} />
                         <span className='block max-w-0 group-hover:max-w-full transition-all duration-500 h-[2px] bg-lightteal group-hover:mt-4 md:group-hover:mt-2 md:duration-300'></span>
                       </PrismicLink>
-                    </li>
-                    <li>
+                    </motion.li>
+                    <motion.li animate={subheaderAnimation}>
                       <PrismicLink field={slice.primary.contact_link} className='group text-lightteal transition duration-300'>
                         <span className='md:border-[1px] md:px-5 md:py-2 md:rounded-full md:border-darkteal md:hover:border-opacity-20 transition duration-0 md:hover:duration-300 md:bg-darkteal md:border-opacity-10 md:bg-opacity-20 md:backdrop-blur-[15px] md:hover:border-lightteal'>{slice.primary.Contact_Button}</span>
                         <span className='block max-w-0 group-hover:max-w-full transition-all duration-500 h-[2px] bg-lightteal group-hover:mt-4 md:group-hover:mt-0 md:h-0'></span>
                       </PrismicLink>
-                    </li>
+                    </motion.li>
                     
                   </ul>
                 </nav>
 
-                <button className='ml-6 md:hidden'
+                <button   className='ml-6 md:hidden'
                   onClick={() => setHamburgerMenuIsOpen((open) => !open)}
                 >
-                  <span className='sr-only'>Toggle menu</span>
+                  <span  className='sr-only'>Toggle menu</span>
                   <HamburgerIcon />
                 </button>
                 </Container>
--- slices/HeroSlice/index.js
+++ slices/HeroSlice/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PrismicRichText } from '@prismicio/react'
+import { PrismicRichText, PrismicLink } from '@prismicio/react'
 import { PrismicNextImage } from '@prismicio/next'
 import RichText from '@/components/RichText'
 import CustomLink from '@/components/CustomLink'
@@ -20,33 +20,29 @@ const HeroSlice = ({ slice }) => (
       </div>
     </div>
       <Container className='pt-[var(--navigation-height)]'>
-        <div className='relative text-center h-[50vh]'>
+        <div className='relative text-center h-[50vh] flex justify-center'>
           <div className='absolute text-center h-full mx-auto justify-center items-center flex flex-col'>
             <RichText field={slice.primary.title} className='text-6xl md:text-8xl font-bold md:text-teal animate-fade-in [--animation-delay:200ms] opacity-0 translate-y-[-1rem] uppercase text-lightteal bg-blend-overlay w-100%' />
             <RichText field={slice.primary.description} className='text-lg md:text-teal text-lightteal md:mt-2 mt-5 animate-fade-in [--animation-delay:400ms] opacity-0 translate-y-[-1rem]'/>
           
           <div className='flex gap-10'>
-            <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" className='mt-8 inline-flex rounded-lg bg-teal text-lg text-background px-5 py-2 animate-fade-in [--animation-delay:600ms] opacity-0 translate-y-[-1rem] hover:bg-mediumteal transition duration-150 ease-in-out'>
-              About
-            </button>
-            <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" className='mt-8 inline-flex rounded-lg border-solid border-2 border-teal text-lg text-teal px-5 py-2 animate-fade-in [--animation-delay:600ms] opacity-0 translate-y-[-1rem] hover:border-mediumteal transition duration-150 ease-in-out'>
-              E-Mail
-            </button>
+            
             
             
             
 
 
-              {/* {
+               {
                 slice.items.map((item, i) => (
-                  <CustomLink
-                    link={item.cta_link}
-                    text={item.cta_text}
+                  <PrismicLink
+                    field={item.cta}
                     key={i}
                     className='mt-8  inline-flex rounded-lg bg-teal text-lg text-background px-5 py-2 animate-fade-in [--animation-delay:600ms] opacity-0 translate-y-[-1rem]'
-                  />
+                  >
+                    <span >{ item.cta_text }</span>
+                  </PrismicLink>
                 ))
-              } */}
+              } 
             
             </div>
           </div>
